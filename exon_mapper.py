5beac51b0c08a37ec5282957b9f7bed1b2e24782
PiraMew <55989422+PiraMew@users.noreply.github.com>
2021-05-27 19:18:00 +0200

    Update exon_mapper.py

--- exon_mapper.py
+++ exon_mapper.py
@@ -10,11 +10,16 @@ parser.add_argument("--outdir", default="alignments_exon")
 args = parser.parse_args()
 outdir = args.outdir
 
+# set directory variables, start at the global working directory
+gwd = os.getcwd()
+hybpiperdirectory = str(gwd+"/steps/hybpiper")
 
 # import recovery statistics
-df = pd.read_csv("/faststorage/project/Maesa/steps/HybPip/mega353_complete/output/seq_lengths.txt", sep="\t")
+df = pd.read_csv(hybpiperdirectory+"/output/seq_lengths.txt", sep="\t")
 df = df.drop(0)
 
+#change directory to ./steps/MAFFT/alignedseq
+os.chdir(gwd+"steps/MAFFT/alignedseq")
 
 # for each aligned locus, identify the two samples with the highest recovery stats
 for fn in os.listdir(): #list file in current directory
@@ -25,11 +30,11 @@ for fn in os.listdir(): #list file in current directory
 		df_red = df_red.reset_index()
 		# first sample
 		sp = df_red["Species"][0]
-		exon1 = list(SeqIO.parse("/faststorage/project/Maesa/steps/HybPip/mega353_complete/"+sp+"/"+locus+"/"+sp+"/sequences/FNA/"+locus+".FNA", "fasta"))[0]
+		exon1 = list(SeqIO.parse(hybpiperdirectory+"/"+sp+"/"+locus+"/"+sp+"/sequences/FNA/"+locus+".FNA", "fasta"))[0]
 		exon1.id = "exon1"
 		# second sample
 		sp = df_red["Species"][1]
-		exon2 = list(SeqIO.parse("/faststorage/project/Maesa/steps/HybPip/mega353_complete/"+sp+"/"+locus+"/"+sp+"/sequences/FNA/"+locus+".FNA", "fasta"))[0]
+		exon2 = list(SeqIO.parse(hybpiperdirectory+"/"+sp+"/"+locus+"/"+sp+"/sequences/FNA/"+locus+".FNA", "fasta"))[0]
 		exon2.id = "exon2"
 		with open("temp.fasta", "w") as output_handle:
 			SeqIO.write([exon1, exon2], output_handle, "fasta")
