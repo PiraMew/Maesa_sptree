45878e78323c929f9f7f52eb759a324ac3f25b0a
PiraMew <55989422+PiraMew@users.noreply.github.com>
2021-05-27 21:49:48 +0200

    Create optrimal_prepare.sh

---
+++ optrimal_prepare.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+#SBATCH --account=Maesa
+#SBATCH --job-name=optrimal_prepare
+#SBATCH --time=00-03:00:00
+#SBATCH --mem-per-cpu=5G
+#SBATCH --cpus-per-task=4
+
+##################################
+# Project: Maesa Phylogeny
+# script: optrimal_prepare.sh
+# --- Action: prepare alignments for optrimAL
+# --- Input: alignments
+# --- Output: alignments with n-replacing
+# USAGE: sbatch optrimal_prepare.sh
+# Author: Pirada Sumanon (pirada.sumanon@bio.au.dk) 
+# Date: 27/05/2021
+##################################
+
+###################
+#---DIRECTORIES---#
+###################
+
+#working directories
+GWD=$PWD #global working directory (main directory) with subproject and scripts
+WD=$PWD/steps #current working directory
+
+#make new directories
+mkdir -p $WD/optrimal_prepare
+
+#copy alignments to new directory
+cp $WD/MAFFT/alignment_exon/*.fasta $WD/optrimal_prepare
+
+#replace n's with gaps (-) in alignments
+for f in *.fasta; do (python $WD/scripts/replace_n.py); done
