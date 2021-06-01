0a792ff5e1599617bd5a1e8de7e1ca428e50769b
PiraMew <55989422+PiraMew@users.noreply.github.com>
2021-06-01 21:58:13 +0200

    Update iqtree_prepare.sh

--- iqtree_prepare.sh
+++ iqtree_prepare.sh
@@ -25,14 +25,29 @@
 GWD=$PWD #global working directory (main directory) with subproject and scripts
 WD=$PWD/steps/alignments_edited #current working directory
 
-cd $WD
+#make new directories
+mkdir -p $WD/noempty
+mkdir -p $PWD/steps/addedOGexons
 
 #######################
 #---EXECUTE SCRIPTS---#
 #######################
-
+cd $WD
 #remove empty sequence(s) from the alignment
 for f in *.aln; do ($GWD/scripts/remove_empty.py $f); done
+mv *_noempty.fasta $WD/noempty
+
+#remove outgroup sequences and add outgroup exons to the alignments
+cd $WD/noempty
+python $GWD/scripts/removeOG.py
+python $GWD/scripts/addOGexon.py
+
+for f in *_OGexon.fasta;
+do (mafft --keeplength --add $f --auto ${f/_OGexon.fasta}_aligned_trimmed_noOG.fasta > ${f/.fasta}added_aligned.fasta);
+done
+
+cp *OGexonadded_aligned.fasta $PWD/steps/addedOGexons
 
 #generate partition files and remove exon sequences
+cd $PWD/steps/addedOGexons
 $GWD/scripts/partitioner_v2.py --smoother 10
